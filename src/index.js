4667cc4101d4075d09525f4ca9aa05dc858d276e
buiminhhai1 <ngovietduc20088@gmail.com>
2019-08-08 17:42:25 +0700

    add POST GET and DELETE method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // You can edit request here.
+    return request;
+}, err => {
+    console.log(err);
+    return Promise.reject(err);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // You can edit response here.
+    return response;
+}, err => {
+    console.log(err);
+    return Promise.reject(err);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
